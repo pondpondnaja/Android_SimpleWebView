ec24695af5514474017a8570d591c6fbd63c09fe
Petch <giggabome@gmail.com>
2020-03-09 19:19:12 +0700

    Simple WebView 9/Mar/2020 19:10

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.webview;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.web_view);
+        webView.setWebViewClient(new CustomWebViewClient());
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDisplayZoomControls(true);
+
+        webView.loadUrl("https://www.youtube.com/watch?v=kstzuU2YdyM");
+    }
+
+    private class CustomWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
